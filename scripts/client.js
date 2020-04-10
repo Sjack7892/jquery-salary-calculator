956ba34043dc559effcc98b1dbedfba59f803c88
Steve Jackson <sjack7892@gmail.com>
2020-04-10 16:59:11 -0500

    pushed inputs to array

--- scripts/client.js
+++ scripts/client.js
@@ -4,5 +4,35 @@ $(document).ready(readyNow);
 
 function readyNow() {
     console.log('jQuery ready!');
-    
+    $('#submitBtn').on('click', submit);
+
 }
+
+let employees = [];
+
+function submit() {
+    console.log('Submit Button Clicked!');
+    //create object
+    let employeeObject = {
+        firstName: $('#fNameInput').val(),
+        lastName: $('#lNameInput').val(),
+        ID: $('#IDInput').val(),
+        title: $('#titleInput').val(),
+        annualSalary: $('#salaryInput').val()
+    }
+
+    $('#fNameInput').val('');
+    $('#lNameInput').val('');
+    $('#IDInput').val('');
+    $('#titleInput').val('');
+    $('#salaryInput').val('');
+
+    console.log(employeeObject);
+    employees.push(employeeObject);
+    // let fName = $('#fNameInput').val();
+    // console.log(fName);
+    console.log(employees);
+}
+
+
+
