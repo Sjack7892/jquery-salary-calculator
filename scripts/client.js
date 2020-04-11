1e74eb670dda5df094e881585bedecf7939b83e1
Steve Jackson <sjack7892@gmail.com>
2020-04-10 23:41:39 -0500

    added displayEmployee

--- scripts/client.js
+++ scripts/client.js
@@ -5,7 +5,6 @@ $(document).ready(readyNow);
 function readyNow() {
     console.log('jQuery ready!');
     $('#submitBtn').on('click', submit);
-
 }
 
 let employees = [];
@@ -17,14 +16,22 @@ function submit() {
     || $('#titleInput').val() === '' || $('#salaryInput').val() === '') {
         alert('Please fill in missing info!');
     }
-    
+
+firstName = $('#fNameInput').val();
+lastName = $('#lNameInput').val()
+ID = $('#IDInput').val();
+title = $('#titleInput').val();
+annualSalary = $('#salaryInput').val();
+
     let employeeObject = {
-        firstName: $('#fNameInput').val(),
-        lastName: $('#lNameInput').val(),
-        ID: $('#IDInput').val(),
-        title: $('#titleInput').val(),
-        annualSalary: $('#salaryInput').val()
+        firstName: firstName,
+        lastName: lastName,
+        ID: ID,
+        title: title,
+        annualSalary: annualSalary
     }
+
+displayEmployee();
 
     $('#fNameInput').val('');
     $('#lNameInput').val('');
@@ -34,9 +41,18 @@ function submit() {
 
     console.log(employeeObject);
     employees.push(employeeObject);
-    // let fName = $('#fNameInput').val();
-    // console.log(fName);
     console.log(employees);
+}
+
+function displayEmployee() {
+    $('#tableData').append(`<tr><td>${firstName}</td><td>${lastName}</td>
+    <td>${ID}</td><td>${title}</td><td>$${annualSalary}</td>
+    <td><button class="btn btn-danger" type="button">Delete</button></td></tr>`);
+
+let totalMonthly = annualSalary / 12;
+console.log(annualSalary);
+
+    $('#totalMonthly').append(` $${totalMonthly}`);
 }
 
 
