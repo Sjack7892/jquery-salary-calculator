7ef4a782b3013ce544c57545098e144619be3f46
Steve Jackson <sjack7892@gmail.com>
2020-04-11 16:00:01 -0500

    added delete

--- scripts/client.js
+++ scripts/client.js
@@ -5,33 +5,39 @@ $(document).ready(readyNow);
 function readyNow() {
     console.log('jQuery ready!');
     $('#submitBtn').on('click', submit);
+    $('#tableData').on('click', '#deleteBtn', deleteRow);
 }
+
 
 let employees = [];
 
+function deleteRow() {
+    console.log('Delete button clicked!');
+    $(this).parent().parent().remove();
+    
+}
+
 function submit() {
-    console.log('Submit Button Clicked!');
     // Check for empty input.
-    if ($('#fNameInput').val() === '' || $('#lNameInput').val() === '' || $('#IDInput').val() === '' 
-    || $('#titleInput').val() === '' || $('#salaryInput').val() === '') {
+    if ($('#fNameInput').val() === '' || $('#lNameInput').val() === '' || $('#IDInput').val() === ''
+        || $('#titleInput').val() === '' || $('#salaryInput').val() === '') {
         alert('Please fill in missing info!');
     }
 
-firstName = $('#fNameInput').val();
-lastName = $('#lNameInput').val()
-ID = $('#IDInput').val();
-title = $('#titleInput').val();
-annualSalary = $('#salaryInput').val();
+    firstName = $('#fNameInput').val();
+    lastName = $('#lNameInput').val()
+    ID = $('#IDInput').val();
+    title = $('#titleInput').val();
+    annualSalary = $('#salaryInput').val();
 
+    // Create object for input to be stored in.
     let employeeObject = {
-        firstName: firstName,
-        lastName: lastName,
-        ID: ID,
-        title: title,
-        annualSalary: annualSalary
+        firstName: $('#fNameInput').val(),
+        lastName: $('#lNameInput').val(),
+        ID: $('#IDInput').val(),
+        title: $('#titleInput').val(),
+        annualSalary: $('#salaryInput').val()
     }
-
-displayEmployee();
 
     $('#fNameInput').val('');
     $('#lNameInput').val('');
@@ -39,21 +45,59 @@ displayEmployee();
     $('#titleInput').val('');
     $('#salaryInput').val('');
 
+    displayEmployee();
+
     console.log(employeeObject);
     employees.push(employeeObject);
     console.log(employees);
+
+    calculateTotalMonthly();
+
+    // $('#deleteBtn').on('click', deleteRow);
 }
+
+
 
 function displayEmployee() {
-    $('#tableData').append(`<tr><td>${firstName}</td><td>${lastName}</td>
-    <td>${ID}</td><td>${title}</td><td>$${annualSalary}</td>
-    <td><button class="btn btn-danger" type="button">Delete</button></td></tr>`);
+    // for ( let i = 0; i < employees.length; i++) {
+    //     $('#tableData').append(`<tr><td>${employees[i].firstName}</td><td>${employees[i].lastName}</td>
+    // <td>${employees[i].ID}</td><td>${employees[i].title}</td><td>$${employees[i].annualSalary}</td>
+    // <td><button class="btn btn-danger" type="button">Delete</button></td></tr>`);
+    // }
 
-let totalMonthly = annualSalary / 12;
-console.log(annualSalary);
 
-    $('#totalMonthly').append(` $${totalMonthly}`);
+    $('#tableData').append
+    (`<tr><td>${firstName}</td>
+    <td>${lastName}</td>
+    <td>${ID}</td>
+    <td>${title}</td>
+    <td>$${formatNumber(annualSalary)}</td>
+    <td id="delete"><button id="deleteBtn" class="btn btn-danger" type="button">Delete</button></td></tr>`);
+
+    
 }
+
+function calculateTotalMonthly() {
+    let totalMonthly = 0;
+
+    for (let i = 0; i < employees.length; i++) {
+        console.log('individual yearly salary is ' + employees[i].annualSalary);
+        let individualMonthlySalary = employees[i].annualSalary / 12;
+        console.log('individual monthly salary is ' + individualMonthlySalary);
+        totalMonthly += individualMonthlySalary;
+
+        console.log('total monthly salary is ' + totalMonthly);
+    }
+   
+    $('#totalMonthlyOutput').empty().append(` $${formatNumber(totalMonthly.toFixed(2))}`);
+
+}
+
+function formatNumber(num) {
+    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+  }
+
+  
 
 
 
