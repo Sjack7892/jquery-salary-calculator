75a0bbe21e8ab2ec078fd8715b5abe2dd9c8c531
Steve Jackson <sjack7892@gmail.com>
2020-04-12 15:34:24 -0500

    added description

--- README.md
+++ README.md
@@ -1,12 +1,18 @@
-# Project Name
-
-[Project Instructions](./INSTRUCTIONS.md), this line may be removed once you have updated the README.md
+# Salary Calculator
 
 ## Description
 
-Your project description goes here. What problem did you solve? How did you solve it?
+This program allows the user to input info on employees and have it display in a table. 
+After the info is displayed, the user is also able to delete it. While adding or deleting
+employee info, the total monthly output will update. If the total monthly output exceeds
+$20,000 it will turn red.
 
-Additional README details can be found [here](https://github.com/PrimeAcademy/readme-template/blob/master/README.md).
+I used objects to hold the value of the user input. I then stored the objects in an array and created 
+a function to loop through them and determine the total monthly salary of all the employees combined 
+whenever input is added or deleted. In order to delete the correct data from the DOM and array I used jQuery 
+to traverse the html and find the index of the corresponding element. 
 
-[x] create html layout
-[] create array and push input to array
+
+
+
+
--- scripts/client.js
+++ scripts/client.js
@@ -8,24 +8,27 @@ function readyNow() {
     $('#tableData').on('click', '#deleteBtn', deleteRow);
 }
 
+// Created array for employee objects.
 let employees = [];
 
 function submit() {
     console.log('Employee info added!');
-    // Check for empty input.
+
+    // Checked for empty input.
     if ($('#fNameInput').val() === '' || $('#lNameInput').val() === '' || $('#IDInput').val() === ''
         || $('#titleInput').val() === '' || $('#salaryInput').val() === '') {
         alert('Please fill in missing info!');
         return;
     }
 
+    // Created variables to append to DOM.
     firstName = $('#fNameInput').val();
     lastName = $('#lNameInput').val();
     iD = $('#IDInput').val();
     title = $('#titleInput').val();
     annualSalary = $('#salaryInput').val();
 
-    // Create object for input to be stored in.
+    // Created object for input to be stored in.
     let employeeObject = {
         firstName: $('#fNameInput').val(),
         lastName: $('#lNameInput').val(),
@@ -34,31 +37,17 @@ function submit() {
         annualSalary: Number($('#salaryInput').val())
     }
 
+    // Emptied input values.
     $('#fNameInput').val('');
     $('#lNameInput').val('');
     $('#IDInput').val('');
     $('#titleInput').val('');
     $('#salaryInput').val('');
 
+    // Pushed object to employees array.
     employees.push(employeeObject);
-    displayEmployee();
-   
 
-    calculateTotalMonthly();
-
-}
-
-function deleteRow() {
-    console.log('Employee info deleted!');
-    let itemToDelete = (this).closest('tr');
-    let index = $(this).parent().parent().index();
-    index -= 1;
-    employees.splice(index, 1);
-    itemToDelete.remove();
-    calculateTotalMonthly()
-}
-
-function displayEmployee() {
+    // Appended variables and table data to DOM.
     $('#tableData').append
     (`<tr>
     <td>${firstName}</td>
@@ -67,26 +56,52 @@ function displayEmployee() {
     <td>${title}</td>
     <td>$<span id="annual">${formatNumber(annualSalary)}</span></td>
     <td id="delete"><button id="deleteBtn" type="button">Delete</button></td>
-    </tr>`); 
+    </tr>`);
+    
+    calculateTotalMonthly();
+}
+
+function deleteRow() {
+    console.log('Employee info deleted!');
+
+    // Declared variable of data to delete from DOM.
+    let itemToDelete = (this).closest('tr');
+
+    // Traversed the DOM to find the index of corresponding html.  
+    let index = $(this).parent().parent().index();
+    index -= 1;
+
+    // Removed corresponding index from emplyees array.
+    employees.splice(index, 1);
+
+    // Deleted variable data from DOM.
+    itemToDelete.remove();
+
+    calculateTotalMonthly()
 }
 
 function calculateTotalMonthly() {
     console.log(employees);
+    
+    // Reset totalMonthly variable.
     totalMonthly = 0;
+
+    // Looped though array to add up annualSalary of each employee.
     for (let i = 0; i < employees.length; i++) {
         totalMonthly += employees[i].annualSalary;
     }
     totalMonthly /= 12;
 
+    // If else to determine result for a totalMonthly over $20,000.
     if (totalMonthly > 20000) {
         alert('Total monthly value has exceded maximum!');
         $('#totalMonthlyOutput').empty().append(`<span id="red" class="badge badge-secondary">$${formatNumber(totalMonthly.toFixed(2))}</span>`);
-        
     } else {
         $('#totalMonthlyOutput').empty().append(`<span id="green" class="badge badge-secondary">$${formatNumber(totalMonthly.toFixed(2))}</span>`);
     }
 }
 
+// Function to add commas for currency values.
 function formatNumber(num) {
     return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
 }
